4895305cdb1da4360ac6eaa3b9d2f25a98ff15cd
Analox <114465675+TomShir@users.noreply.github.com>
2024-01-02 22:40:11 +0000

    Add files via upload

---
+++ txt.py
@@ -0,0 +1,13 @@
+import turtle
+while True:
+  for c in['red','turquoise','yellow','blue','green','black']:
+    anomaly=turtle
+    anomaly.color(c)
+    anomaly.forward(90)
+    anomaly.left(90)
+    anomaly.backward(45)
+    anomaly.left(90)
+    anomaly.forward(90)
+    anomaly.right(90)
+    anomaly.forward(90)
+    anomaly.left(60)
